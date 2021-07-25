e1bb41e4ba600c653c76c1908aaa919ddb38ed89
patrickhaussmann <git@patrickhaussmann.de>
2021-07-25 19:05:29 +0200

    Add table for all warncellids

---
+++ get_warncellids.py
@@ -0,0 +1,10 @@
+﻿#!/usr/bin/env python
+
+import pandas as pd
+
+url = "https://www.dwd.de/DE/leistungen/opendata/help/warnungen/cap_warncellids_csv.csv?__blob=publicationFile&v=3"
+df = pd.read_csv(url, sep=";")
+df = df[~df['KENNUNG (NUTS)'].isnull()]
+df = df.drop(['KENNUNG (NUTS)', 'KENNUNG (SIGN)', 'KURZNAME'], axis=1)
+
+df.to_csv('warncellids.csv', index=False)
