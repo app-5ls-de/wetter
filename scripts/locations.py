f4e138b20472c92ce3925c8915a8b98f7e0de746
patrickhaussmann <git@patrickhaussmann.de>
2021-08-06 15:00:43 +0200

    Fix: Use absolute paths in scripts

--- scripts/locations.py
+++ scripts/locations.py
@@ -2,19 +2,21 @@
 
 import yaml
 import json
-from os import walk
+from os import walk, path
+
+main_path = path.realpath(path.join(path.dirname(__file__), ".."))
 
 list_locations = []
-directory ="_locations/"
+locations_path = path.join(main_path, "_locations/")
 
 # from https://stackoverflow.com/a/3207973
-_, _, filenames = next(walk(directory))
+_, _, filenames = next(walk(locations_path))
 
 for location in filenames:
-    with open("_locations/"+location, "r") as f:
+    with open(path.join(locations_path, location), "r") as f:
         data_yaml, data_md = f.read()[3:].split("\n---\n")
         try:
-            parsed_yaml= yaml.safe_load(data_yaml)
+            parsed_yaml = yaml.safe_load(data_yaml)
             location_dict = {
                 "name": location[:-3],
                 "data": parsed_yaml,
@@ -25,7 +27,7 @@ for location in filenames:
         except yaml.YAMLError as exc:
             print(exc)
 
-list_locations = sorted(list_locations, key=lambda k: k['name']) 
+list_locations = sorted(list_locations, key=lambda k: k['name'])
 
-with open('locations.json', 'w', encoding='utf-8') as f:
+with open(path.join(main_path, 'locations.json'), 'w', encoding='utf-8') as f:
     json.dump(list_locations, f, ensure_ascii=False, indent=2)
--- scripts/warncellids.py
+++ scripts/warncellids.py
@@ -1,10 +1,12 @@
 ﻿#!/usr/bin/env python
 
 import pandas as pd
+from os import path
 
 url = "https://www.dwd.de/DE/leistungen/opendata/help/warnungen/cap_warncellids_csv.csv?__blob=publicationFile&v=3"
 df = pd.read_csv(url, sep=";")
 df = df[~df['KENNUNG (NUTS)'].isnull()]
 df = df.drop(['KENNUNG (NUTS)', 'KENNUNG (SIGN)', 'KURZNAME'], axis=1)
 
-df.to_csv('warncellids.csv', index=False)
+main_path = path.realpath(path.join(path.dirname(__file__), ".."))
+df.to_csv(path.join(main_path, 'warncellids.csv'), index=False)
