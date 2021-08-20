34f6eabc72f15226cfdbcfec7ab2c9ff3148254e
patrickhaussmann <git@patrickhaussmann.de>
2021-08-21 00:47:48 +0200

    Better service-worker with Workbox

--- service-worker.js
+++ service-worker.js
@@ -1,90 +1,71 @@
----
----
-const PRECACHE = 'precache-v{{ "now" | date: "%s"}}';
-const RUNTIME = "runtime";
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js"
+);
+const { registerRoute, setDefaultHandler } = workbox.routing;
+const { StaleWhileRevalidate, CacheFirst } = workbox.strategies;
+const { ExpirationPlugin } = workbox.expiration;
+const { cacheNames } = workbox.core;
 
-const PRECACHE_URLS = [
-    "/",
-    "/index.html",
-    "/show",
-    "/locations.json",
-    "/404.html",
-    "/style.css",
-    "/manifest.json",
-    "/favicon.ico",
-    "/info.svg",
-    "/icon.svg",
-    "/sunset.svg",
-    "/sunrise.svg",
-    "/script.js",
-    "https://cdn.jsdelivr.net/npm/fuzzysort@1.1.4/fuzzysort.min.js",
-    "/back.svg",
-    "search.svg",
-    "/icons/android-chrome-192x192.png",
-    "/icons/android-chrome-512x512.png",
-    "/icons/maskable_icon.png",
-    "/icons/apple-touch-icon.png",
-    "/icons/favicon-32x32.png",
-    "/icons/favicon-16x16.png",
-    "/icons/safari-pinned-tab.svg",
-    "/icons/browserconfig.xml",
-    "/icons/mstile-70x70.png",
-    "/icons/mstile-150x150.png",
-    "/icons/mstile-310x310.png",
-];
+/* workbox.setConfig({
+  debug: false,
+}); */
+
+async function cacheKeyWillBeUsed({ request }) {
+  const url = new URL(request.url || request);
+  // Any search params or hash will be left out.
+  return url.origin + url.pathname;
+}
+
+registerRoute(
+  ({ url }) => url.origin == location.origin,
+  new StaleWhileRevalidate({
+    plugins: [{ cacheKeyWillBeUsed }],
+  })
+);
+
+registerRoute(
+  ({ url }) =>
+    [
+      "https://cdn.jsdelivr.net",
+      "https://nominatim.openstreetmap.org",
+    ].includes(url.origin),
+  new CacheFirst()
+);
+
+registerRoute(
+  ({ url }) =>
+    ["https://cdn.knmi.nl", "https://api.sunrise-sunset.org"].includes(
+      url.origin
+    ),
+  new CacheFirst({
+    cacheName: "expiration-cache",
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 24 * 60 * 60,
+        matchOptions: {
+          ignoreVary: true,
+        },
+      }),
+    ],
+  })
+);
+
+setDefaultHandler(
+  new StaleWhileRevalidate({
+    cacheName: "expiration-cache",
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 1 * 60 * 60,
+        matchOptions: {
+          ignoreVary: true,
+        },
+      }),
+    ],
+  })
+);
 
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches
-            .open(PRECACHE)
-            .then((cache) => cache.addAll(PRECACHE_URLS))
-            .then(self.skipWaiting())
-    );
-});
-
-self.addEventListener("activate", (event) => {
-    const currentCaches = [PRECACHE];
-    event.waitUntil(
-        caches
-            .keys()
-            .then((cacheNames) => {
-                return cacheNames.filter(
-                    (cacheName) => !currentCaches.includes(cacheName)
-                );
-            })
-            .then((cachesToDelete) => {
-                return Promise.all(
-                    cachesToDelete.map((cacheToDelete) => {
-                        return caches.delete(cacheToDelete);
-                    })
-                );
-            })
-            .then(() => self.clients.claim())
-    );
-});
-
-self.addEventListener("fetch", (event) => {
-    if (
-        event.request.url.startsWith(self.location.origin) ||
-        event.request.url.startsWith("https://cdn.jsdelivr.net/")
-    ) {
-        event.respondWith(
-            caches
-                .match(event.request, { ignoreSearch: true })
-                .then((cachedResponse) => {
-                    if (cachedResponse) {
-                        return cachedResponse;
-                    }
-                    return caches.open(RUNTIME).then((cache) => {
-                        return fetch(event.request).then((response) => {
-                            return cache
-                                .put(event.request, response.clone())
-                                .then(() => {
-                                    return response;
-                                });
-                        });
-                    });
-                })
-        );
-    }
+  const urls = ["/", "/show", "/404.html"];
+  const cacheName = cacheNames.runtime;
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(urls)));
 });
