0c158d2fe989dd6a92978e87afb4c08887af2fe9
patrickhaussmann <git@patrickhaussmann.de>
2021-09-21 21:13:48 +0200

    Sort locations

--- scripts/locations.py
+++ scripts/locations.py
@@ -14,7 +14,7 @@ with open(location_path) as fd:
         array.append(dict(sorted(element.items())))
 
 array = sorted(array, key=lambda k: k['name'])
-array = sorted(array, key=lambda k: len(k), reverse=True)
 
 with open(location_path, 'w', encoding='utf-8') as f:
     json.dump(array, f, ensure_ascii=False, indent=2)
+    f.write("\n")
