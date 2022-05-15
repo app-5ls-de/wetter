6c706d6e459debef25c1c4fa044bf5ef82cd77f5
patrickhaussmann <git@patrickhaussmann.de>
2022-05-15 18:14:12 +0200

    Load real weather data

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ async function fetch_json(url) {
   return data;
 }
 
+function Place(name, countryCode, lat, lon) {
+  this.name = name;
+  this.countryCode = countryCode;
+  this.lat = lat;
+  this.lon = lon;
+}
+
+const places = [new Place("Aarhus", "dk", 56.16, 10.21)];
+
 crel.attrMap["cssVariable"] = (element, value) => {
   for (let varName in value) {
     if (Object.hasOwnProperty.call(value, varName)) {
@@ -15,189 +24,241 @@ crel.attrMap["cssVariable"] = (element, value) => {
   }
 };
 
-async function openweathermap(lat = 50, lon = 10) {
-  const apiKey = "4fbc2ce2fc600e6" + /*  */ "e450dd4bbde8f28be";
+const msToBeaufort = (ms) => Math.round(Math.cbrt(Math.pow(ms / 0.836, 2)));
+
+function weatherConditionNameFromId(id, icon) {
+  const dayNight = icon.includes("d") ? "day" : "night";
+  if (id >= 200 && id <= 232) {
+    return "thunderstorms" + dayNight;
+  } else if (id >= 300 && id <= 321) {
+    return "partly-cloudy-" + dayNight + "-drizzle";
+  } else if (id >= 500 && id <= 531) {
+    return "partly-cloudy-" + dayNight + "-rain";
+  } else if (id >= 600 && id <= 622) {
+    return "partly-cloudy-" + dayNight + "-snow";
+  } else if (id >= 701 && id <= 781) {
+    return "partly-cloudy-" + dayNight + "-fog";
+  } else if (id === 800) {
+    return "clear-" + dayNight;
+  } else if (id === 801) {
+    return "partly-cloudy-" + dayNight;
+  } else if (id === 802) {
+    return "cloudy";
+  } else if (id === 803 || id === 804) {
+    return "overcast-" + dayNight;
+  } else {
+    return "not-available";
+  }
+}
+
+async function openweathermap(place) {
+  const apiKey =
+    "4fbc2ce2fc600e6" + /* if you're a bot, fuck off */ "e450dd4bbde8f28be";
   const data = await fetch_json(
-    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-      lat +
+    "https://api.openweathermap.org/data/2.5/onecall?units=metric&lang=en&lat=" +
+      place.lat +
       "&lon=" +
-      lon +
+      place.lon +
       "&appid=" +
       apiKey
   );
   return data;
 }
 
-function createCityBox() {
+function createCityBox(place) {
+  const divBox = crel.a({
+    class: "box city p-0",
+  });
   const divColumn = crel.div(
     {
       class: "column is-one-quarter",
     },
-    (divBox = crel.a({
-      class: "box city p-0",
-    }))
+    divBox
   );
   divCities.appendChild(divColumn);
 
-  // fetch data
+  openweathermap(place).then((data) => {
+    const divBlock = crel.div({
+      class: "block pl-3 pr-3 m-0",
+    });
+    divBox.appendChild(divBlock);
 
-  const divBlock = crel.div({
-    class: "block pl-3 pr-3 m-0",
-  });
-  const divLevelName = crel.div(
-    {
-      class: "level pt-3 pb-3",
-    },
-    crel.h2(
+    const divLevelName = crel.div(
       {
-        class: "city-name level-item",
+        class: "level pt-3 pb-3",
       },
-      crel.span(
+      crel.h2(
         {
-          class: "is-size-5 has-text-grey",
+          class: "city-name level-item",
         },
-        "Ulm"
-      ),
-      crel.sup(
-        {
-          class: "is-size-7 mb-4 is-uppercase",
-        },
-        "de"
-      )
-    )
-  );
-  divBlock.appendChild(divLevelName);
-
-  const divLevelIcons2 = crel.div(
-    {
-      class: "level city-icons is-relative",
-    },
-    crel.img({
-      class: "city-icon2 level-item",
-      style: { width: "7rem", left: "0.5rem", top: "-2rem" },
-      src: "https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/rainy-1.svg",
-      alt: "rainy-1",
-    }),
-    crel.img({
-      class: "city-icon2 level-item",
-      style: { width: "4rem", right: 0, top: "-1rem" },
-      src: "https://raw.githubusercontent.com/basmilius/weather-icons/dev/production/fill/svg/wind-beaufort-1.svg",
-      alt: "wind-beaufort-1",
-    })
-  );
-  divBlock.appendChild(divLevelIcons2);
-
-  const divLevelTempMax = crel.div(
-    {
-      class: "level-item mb-0 mr-0 is-size-3 level",
-    },
-    crel.img({
-      class: "city-temp-icon level-item",
-      src: "https://raw.githubusercontent.com/basmilius/weather-icons/dev/production/fill/svg-static/thermometer-warmer.svg",
-      alt: "thermometer-warmer",
-    }),
-    crel.div(
-      {
-        class: "city-temp has-text-weight-bold level-item",
-      },
-      "23",
-      crel.div(
-        {
-          class: "city-temp-celsius mb-4 has-text-grey",
-        },
-        "°C"
-      )
-    )
-  );
-  const divLevelTempMin = crel.div(
-    {
-      class: "level-item mb-0 mr-0 is-size-3 level",
-    },
-    crel.img({
-      class: "city-temp-icon level-item",
-      src: "https://raw.githubusercontent.com/basmilius/weather-icons/dev/production/fill/svg-static/thermometer-colder.svg",
-      alt: "thermometer-colder",
-    }),
-    crel.div(
-      {
-        class: "city-temp has-text-weight-bold level-item",
-      },
-      "12",
-      crel.div(
-        {
-          class: "city-temp-celsius mb-4 has-text-grey",
-        },
-        "°C"
-      )
-    )
-  );
-  const divLevelTemp = crel.div(
-    {
-      class: "level is-mobile",
-    },
-    crel.div(
-      {
-        class: "city-temp level-item is-size-1 has-text-weight-bold",
-      },
-      "18",
-      crel.div(
-        {
-          class: "city-temp-celsius mb-4 has-text-grey",
-        },
-        "°C"
-      )
-    ),
-    crel.div(
-      {
-        class: "level-right",
-      },
-      crel.div(divLevelTempMax, divLevelTempMin)
-    )
-  );
-  divBlock.appendChild(divLevelTemp);
-
-  divBox.appendChild(divBlock);
-
-  const isRain = Math.random() > 0.2 ? 1 : 0;
-
-  if (isRain) {
-    const divChart = crel.div(
-      {
-        class: "city-minute level-item",
-      },
-      crel.table(
-        {
-          class: "charts-css column",
-        },
-        (tbodyChart = crel.tbody({
-          class: "charts-css column",
-        }))
+        crel.span(
+          {
+            class: "is-size-5 has-text-grey",
+          },
+          place.name
+        ),
+        crel.sup(
+          {
+            class: "is-size-7 mb-4 is-uppercase",
+          },
+          place.countryCode
+        )
       )
     );
-    for (let i = 0; i < 60; i++) {
-      let size = Math.cos(((2 * i) / 100) * 2 * Math.PI + Math.random() * 0.7);
-      size = Math.max(0, size) * isRain;
-      const tr = crel.tr(
-        crel.td(
+    divBlock.appendChild(divLevelName);
+
+    const beaufort = msToBeaufort(data.current.wind_speed);
+    const weatherCondition = weatherConditionNameFromId(
+      data.current.weather[0].id,
+      data.current.weather[0].icon
+    );
+    const wind_deg = data.current.wind_deg + (180 % 360);
+    const divLevelIcons2 = crel.div(
+      {
+        class: "level city-icons is-relative",
+      },
+      crel.img({
+        class: "city-icon2 level-item",
+        style: { width: "7rem", left: "0.5rem", top: "-2rem" },
+        // alternative: https://www.amcharts.com/free-animated-svg-weather-icons/
+        src:
+          "https://raw.githubusercontent.com/basmilius/weather-icons/dev/production/fill/svg/" +
+          weatherCondition +
+          ".svg",
+        alt: weatherCondition,
+      }),
+      crel.img({
+        class: "city-icon2 level-item",
+        style: {
+          width: "4rem",
+          right: 0,
+          top: "-1rem",
+          "transform-origin": "50% 50%",
+          transform: "rotate(" + wind_deg + "deg)",
+        },
+        src:
+          "https://raw.githubusercontent.com/basmilius/weather-icons/dev/production/fill/svg/wind-beaufort-" +
+          beaufort +
+          ".svg",
+        alt: "wind-beaufort-" + beaufort,
+      })
+    );
+    divBlock.appendChild(divLevelIcons2);
+
+    const divLevelTempDay = crel.div(
+      {
+        class: "level-item mb-0 mr-0 is-size-3 level",
+      },
+      crel.img({
+        class: "city-temp-icon level-item",
+        src: "https://raw.githubusercontent.com/basmilius/weather-icons/dev/production/fill/svg-static/thermometer-warmer.svg",
+      }),
+      crel.div(
+        {
+          class: "city-temp has-text-weight-bold level-item",
+        },
+        data.daily[0].temp.day.toFixed(0),
+        crel.div(
           {
-            cssVariable: {
-              size: size.toFixed(2),
-            },
+            class: "city-temp-celsius mb-4 has-text-grey",
           },
-          size
-            ? crel.span(
-                {
-                  class: "tooltip",
-                },
-                "in " + i + "min"
-              )
-            : undefined
+          "°C"
+        )
+      )
+    );
+    const divLevelTempNight = crel.div(
+      {
+        class: "level-item mb-0 mr-0 is-size-3 level",
+      },
+      crel.img({
+        class: "city-temp-icon level-item",
+        src: "https://raw.githubusercontent.com/basmilius/weather-icons/dev/production/fill/svg-static/thermometer-colder.svg",
+      }),
+      crel.div(
+        {
+          class: "city-temp has-text-weight-bold level-item",
+        },
+        data.daily[0].temp.night.toFixed(0),
+        crel.div(
+          {
+            class: "city-temp-celsius mb-4 has-text-grey",
+          },
+          "°C"
+        )
+      )
+    );
+    const divLevelTemp = crel.div(
+      {
+        class: "level is-mobile",
+      },
+      crel.div(
+        {
+          class: "city-temp level-item is-size-1 has-text-weight-bold",
+        },
+        data.current.temp.toFixed(0),
+        crel.div(
+          {
+            class: "city-temp-celsius mb-4 has-text-grey",
+          },
+          "°C"
+        )
+      ),
+      crel.div(
+        {
+          class: "level-right",
+        },
+        crel.div(divLevelTempDay, divLevelTempNight)
+      )
+    );
+    divBlock.appendChild(divLevelTemp);
+
+    const maxPrecipitation = data.minutely.reduce(
+      (acc, cur) => Math.max(acc, cur.precipitation),
+      0
+    );
+    const maxChartPrecipitation = Math.max(maxPrecipitation, 3);
+
+    if (true || maxPrecipitation) {
+      const divChart = crel.div(
+        {
+          class: "city-minute level-item",
+        },
+        crel.table(
+          {
+            class: "charts-css column",
+          },
+          (tbodyChart = crel.tbody({
+            class: "charts-css column",
+          }))
         )
       );
-      tbodyChart.appendChild(tr);
+      for (let i = 0; i < data.minutely.length; i++) {
+        const size = data.minutely[i].precipitation / maxChartPrecipitation;
+
+        const tr = crel.tr(
+          size
+            ? crel.td(
+                {
+                  cssVariable: {
+                    size: size.toFixed(2),
+                  },
+                },
+                crel.span(
+                  {
+                    class: "tooltip",
+                  },
+                  "in " + i + "min"
+                )
+              )
+            : undefined
+        );
+        tbodyChart.appendChild(tr);
+      }
+      divBox.appendChild(divChart);
     }
-    divBox.appendChild(divChart);
-  }
+  });
 }
 
-createCityBox();
+for (const place of places) {
+  createCityBox(place);
+}
